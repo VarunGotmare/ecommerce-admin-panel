ee0f28b9380c0053f163b5b3fd310bce637cf8f1
Varun Gotmare <varunpgotmare@gmail.com>
2024-03-02 19:04:35 +0530

    orders page

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/cell-action.tsx
@@ -0,0 +1,72 @@
+"use client";
+import { DropdownMenu,DropdownMenuTrigger,DropdownMenuContent, DropdownMenuLabel, DropdownMenuItem } from "@/components/ui/dropdown-menu";
+import {BillboardColumn} from "./columns";
+import { Button } from "@/components/ui/button";
+import { Copy, Edit, MoreHorizontal, MoreHorizontalIcon, Trash } from "lucide-react";
+import toast from "react-hot-toast";
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+import axios from "axios";
+import { AlertModal } from "@/components/modals/alert-modal";
+interface CellActionProps {
+    data: BillboardColumn
+    }
+export const CellAction: React.FC<CellActionProps> = ({
+    data
+}) => {
+    const router = useRouter();
+    const params = useParams();
+    const [open, setOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const onCopy = (id: string) => {
+        navigator.clipboard.writeText(id);
+        toast.success("Copied to clipboard");
+    }
+    const onDelete = async () => {
+        try{
+            setLoading(true);
+            await axios.delete(`/api/${params.storeId}/billboards/${data.id}`);
+            router.refresh();
+            router.push(`/${params.storeId}/billboards`)
+            toast.success("Billboard deleted");
+        }catch(error){
+            toast.error("Make sure you remove all products and categories first.");
+        
+        }finally{
+            setLoading(false);
+            setOpen(false);
+        };
+    }
+    return (
+        <>
+        <AlertModal onClose={()=> setOpen(false)} isOpen={open}  loading={loading} onConfirm={onDelete}/>
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+            <Button variant={"ghost"} className="h-8 w-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontalIcon className="h-4 w-4" />
+            </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuLabel>
+                    Actions
+                </DropdownMenuLabel>
+                <DropdownMenuItem onClick={()=> router.push(`/${params.storeId}/billboards/${data.id}`)}>
+                    <Edit className="h-4 w-4 mr-2"/>
+                    Update
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={()=> onCopy(data.id)}>
+                    <Copy className="h-4 w-4 mr-2"/>
+                    Copy Id
+                </DropdownMenuItem>
+                <DropdownMenuItem  onClick={()=> setOpen(true)}>
+                    <Trash className="h-4 w-4 mr-2"/>
+                    Delete
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    
+    </>
+    );
+}
---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Order } from "@prisma/client";
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import React from "react";
+import { OrderColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
+
+interface OrderClientProps {
+    data: OrderColumn[]
+}
+
+export const OrderClient: React.FC<OrderClientProps> = ({
+    data
+}) => {
+
+    return ( 
+        <>
+            <Heading title={`Orders (${data.length})`} description="Manage Store Orders"/>
+     
+        <Separator />
+        <DataTable columns={columns} data={data} searchKey="products"/>
+        </>
+     );
+}
